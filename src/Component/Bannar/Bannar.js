26578b829ef0aa8c6d20b39aa7a9aa42f9cdb7bc
maksudalam <maksud.cdz@gmail.com>
2022-10-11 07:19:22 +0600

    Banner added

--- src/Component/Bannar/Bannar.js
+++ src/Component/Bannar/Bannar.js
@@ -5,10 +5,10 @@ const Banner = () => {
         <div>
             <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row">
-    <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
+    <img src="https://t3.ftcdn.net/jpg/03/18/60/62/360_F_318606217_Hk8jo2MVoI33SQOkYrfOF929J7JgIP0P.jpg" className="max-w-sm rounded-lg shadow-2xl" />
     <div>
-      <h1 className="text-5xl font-bold">!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <h1 className="text-5xl font-bold">Font-End Developer!!!</h1>
+      <p className="py-6">As I was looking for some useful coding quotes, I discovered many funny ones. Enjoy reading them — I thought these should be compiled together.Most are from other sources (as per linked), with the exception of a handful that are my own.</p>
       <button className="btn btn-primary">Get Started</button>
     </div>
   </div>
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -11,6 +11,7 @@ const Header = () => {
             <div>
                 <Banner></Banner>
             </div>
+            
         </div>
     );
 };
